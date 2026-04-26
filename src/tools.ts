ee164f70c54149ee4642d7e34e78acefe3ace12a
Ross Simpson <ross.simpson@gmail.com>
2026-04-26 18:24:52 +0200

    Very flaky "cli" tool

--- src/tools.ts
+++ src/tools.ts
@@ -1,3 +1,5 @@
+const { exec } = require('child_process')
+
 export type ToolDefinition = {
   name: string;
   description: string;
@@ -20,4 +22,40 @@ export const tools: ToolDefinition[] = [
       return input.text;
     },
   },
+
+  {
+    name: "cli",
+    description: "Executes a Linux CLI command",
+    input_schema: {
+      type: "object",
+      properties: {
+        command: { type: "string" },
+        arguments: { type: "string" },
+      },
+      required: ["command"],
+    },
+    async handler(input) {
+    	console.log("[DEBUG]: Running cli tool: ", input.command);
+    	if (input.arguments) {
+    		console.log("[DEBUG]: Arguments: ", input.arguments);
+    	}
+
+		return new Promise((resolve) => {
+			exec(input.command + " " + input.arguments, (error: Error | null, stdout: string, stderr: string) => {
+			  if (error) {
+			  	console.log("ERROR: ", error.message)
+			    console.log("Returning: " + `Unexpected error running command!`);
+			    return resolve(`Unexpected error running command!`);
+			  }
+			  if (stderr) {
+			    console.log("Returning: " + `Stderr: ${stderr}`);
+			    return resolve(`Stderr: ${stderr}`);
+			  }
+			  console.log("Returning: " + stdout);
+			  return resolve(stdout);
+			});
+		});
+    },
+  },
+
 ];
