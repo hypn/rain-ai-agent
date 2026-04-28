d5f93d2ed77d5cf0b790f185209a9b0d89d7fc99
Ross Simpson <ross.simpson@gmail.com>
2026-04-28 09:30:12 +0200

    v0.2 - refactored logic, removed "echo" Tool, improved conversation debugging, added slash commands

--- src/agent.ts
+++ src/agent.ts
@@ -1,107 +1,175 @@
+import * as readline from "readline";
 import Anthropic from "@anthropic-ai/sdk";
 
 const cliMd = require('cli-markdown').default;
 
-const debug = false;
+let debug = false;
 
 import type { ToolDefinition } from "./tools";
 
-export type GetUserMessage = () => Promise<[string, boolean]>;
-
 export class Agent {
 	private client: Anthropic;
-	private getUserMessage: GetUserMessage;
 	private conversation: Anthropic.Messages.MessageParam[] = [];
 	private tools: ToolDefinition[];
+	private model: string
+	private modelShortName: string
 
-	constructor(client: Anthropic, getUserMessage: GetUserMessage, tools: ToolDefinition[]) {
-		this.client = client;
-		this.getUserMessage = getUserMessage;
+	constructor(apiKey: string, tools: ToolDefinition[]) {
+		this.client = new Anthropic({ apiKey: apiKey });
 		this.tools = tools;
+		this.model = "claude-opus-4-7";
+		this.modelShortName = this.model;
 	}
 
 	async run(): Promise<void> {
-		let skipUserInput = false;
+		let toolResult = false;  // flag whether there is a Tool Use result the LLM still needs to process
 
 		while (true) {
-			// allow a cycle without prompting the user (eg: tool use)
-			if (skipUserInput) {
-				skipUserInput = false;
-			} else {
-				const [msg, ok] = await this.getUserMessage();
-				if (!ok) {
-					break;
+			if (!toolResult) {
+				// Step 1: get user's prompt/input
+				let msg = await this.getUserInput();
+
+				// Step 2: add the user's prompt to the Conversation (see https://platform.claude.com/docs/en/api/sdks/typescript#usage)
+				if (msg) {
+					this.conversation.push({ role: "user", content: msg });
+				} else {
+					continue; // prompt again if no input (eg: slash command used)
 				}
-
-				// see https://platform.claude.com/docs/en/api/sdks/typescript#usage
-
-				// add the user's message to the conversation
-				this.conversation.push({
-					role: "user",
-					content: msg
-				});
 			}
 
+			// Step 3: send the Converation to the LLM
+			const llmResponse = await this.sendConversation();
+			toolResult = false; // regardless of whether there was a Tool result or not, it would have been processed at this point
 
-			// get a response from the AI
-			const message = await this.client.messages.create({
-				max_tokens: 1024,
-				messages: this.conversation,
-				model: "claude-opus-4-7",
-  				tools: this.tools,
-			});
-			if (debug) {
-				console.log(JSON.stringify(this.conversation));
-				console.log("-----------------");
-				console.log();
-			}
-
-			for (const block of message.content) {
+			// Step 4: process LLM response(s)
+			for (const block of llmResponse.content) {
 				if (block.type === "text") {
-					console.log('\x1b[38;5;208mClaude:\x1b[0m', cliMd(block.text).trim(), "\n");
-
-					// ERROR: "This model does not support assistant message prefill. The conversation must end with a user message"
-					// // add the AI's response to the conversation
-					// this.conversation.push({
-					// 	role: "assistant",
-					// 	content: block.text
-					// });
-					this.conversation.push({
-						role: "assistant",
-						content: block.text
-					});
+					// text returned
+					await this.handleTextResponse(block);
 
 				} else if (block.type === "tool_use") {
-					/*
-						{
-							type: 'tool_use',
-							id: 'toolu_01SvoWboiPLQzi3pub5o1tME',
-							name: 'echo',
-							input: { text: 'hello world' },
-							caller: { type: 'direct' }
-						}
-					*/
-					this.conversation.push({
-						role: "assistant", 
-						content: [block]
-					});
-
-					let toolResults = await this.executeTool(block.id, block.name, block.input);
-			  		this.conversation.push({
-						role: "user",
-						content: toolResults
-					});
-					skipUserInput = true;
-
-
-				} else {
-					if (debug) {
-						console.log("[DEBUG] Unknown block.type received: ", block.type);
-					}
+					// tool used
+					await this.handleToolUse(block);
+					toolResult = true;
 				}
 			}
 
+			if (debug) { this.printConversationDebug(); }
 		}
+	}
+
+	private async getUserInput(): Promise<string> {
+		const rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout,
+		});
+
+		const answer = await new Promise<string>((resolve) =>
+			rl.question('\x1b[38;5;80mYou:\x1b[0m ', resolve)
+		);
+		rl.close();
+
+		if (answer && (answer.indexOf("/") == 0)) {
+			await this.slashCommand(answer);
+			console.log();
+			return "";
+
+		} else {
+			console.log();
+			return answer;
+		}
+	}
+
+	private async handleTextResponse(block: any) {
+		// add the LLM's response to the Conversation
+		this.conversation.push({
+			role: "assistant",
+			content: block.text
+		});
+
+		// display LLM text responses
+		console.log('\x1b[38;5;208m' + this.modelShortName + ':\x1b[0m', cliMd(block.text).trim(), "\n");
+	}
+
+	private async handleToolUse(block: any) {
+		/* eg:
+			{
+				type: 'tool_use',
+				id: 'toolu_01SvoWboiPLQzi3pub5o1tME',
+				name: 'echo',
+				input: { text: 'hello world' },
+				caller: { type: 'direct' }
+			}
+		*/
+
+		// add the LLM's tool use to the conversation
+		this.conversation.push({ role: "assistant", content: [block] });
+
+		// execute the tool 
+		let toolResults = await this.executeTool(block.id, block.name, block.input);
+
+		// add the tool's result to the Conversation
+		this.conversation.push({ role: "user", content: toolResults });
+	}
+
+	private async slashCommand(msg: string) {
+		if (msg == "/models") {
+			// list available models
+			console.log("Anthropic Models:")
+			for await (const modelInfo of this.client.models.list()) {
+			  console.log(" * " + modelInfo.id);
+			}
+
+		} else if (msg.startsWith("/model ")) {
+			// change models
+			const model = msg.slice(7).trim();
+			if (model) {
+				console.log("Switching from " + this.model + " to " + model);
+
+				let modelShortName = model.replace("claude-", "");
+				modelShortName = modelShortName.charAt(0).toUpperCase() + modelShortName.substring(1);
+				const idx = modelShortName.indexOf("-202");
+				if (idx !== -1) {
+					modelShortName = modelShortName.slice(0, idx);
+				}
+
+				this.model = model;
+				this.modelShortName = modelShortName;
+			} else {
+				console.log("Please specify a model name!");
+			}
+
+		} else if (msg === "/debug") {
+			debug = !debug;
+			console.log("Debug:", debug);
+
+		} else if (msg === "/help") {
+			console.log("* /model {model} = change active Anthropic model");
+			console.log("* /models        = list available Anthropic models");
+			console.log("* /debug         = enable debug logging");
+			console.log("* /quit          = quit");
+
+		} else if (msg === "/quit") {
+			console.log("Good bye :)");
+			console.log();
+			process.exit(0);
+
+		} else {
+			console.log("Unknown command - try /help");
+		}
+	}
+
+	private async sendConversation() {
+		if (debug) { this.printConversationDebug(); }
+
+		const message = await this.client.messages.create({
+			max_tokens: 1024,
+			messages: this.conversation,
+			model: this.model,
+			tools: this.tools,
+		});
+
+		return message;
 	}
 
 	private async executeTool(id: string, name: string, input: any): Promise<any> {
@@ -147,5 +215,12 @@ export class Agent {
 		}
 
 		return toolResults;
+	}
+
+	private printConversationDebug() {
+		console.log("----------------------------------------");
+		console.log("[DEBUG] Conversation (context):");
+		console.log(' ' + cliMd('```json\n' + JSON.stringify(this.conversation, null, 4), '```').trim());
+		console.log("----------------------------------------");
 	}
 }
--- src/tools.ts
+++ src/tools.ts
@@ -3,47 +3,32 @@ const { exec } = require('child_process')
 const debug = false;
 
 export type ToolDefinition = {
-  name: string;
-  description: string;
-  input_schema: any;
-  handler: (input: any) => Promise<string>;
+	name: string;
+	description: string;
+	input_schema: any;
+	handler: (input: any) => Promise<string>;
 };
 
 export const tools: ToolDefinition[] = [
-  {
-    name: "echo",
-    description: "Returns the same text",
-    input_schema: {
-      type: "object",
-      properties: {
-        text: { type: "string" },
-      },
-      required: ["text"],
-    },
-    async handler(input) {
-      return input.text;
-    },
-  },
+	{
+	name: "cli",
+	description: "Executes a Linux CLI command",
+	input_schema: {
+		type: "object",
+		properties: {
+		command: { type: "string" },
+		arguments: { type: "string" },
+		},
+		required: ["command"],
+	},
+	async handler(input) {
 
-  {
-    name: "cli",
-    description: "Executes a Linux CLI command",
-    input_schema: {
-      type: "object",
-      properties: {
-        command: { type: "string" },
-        arguments: { type: "string" },
-      },
-      required: ["command"],
-    },
-    async handler(input) {
-
-    	if (debug) {
-	    	console.log("[DEBUG]: Running cli tool: ", input.command);
-	    	if (input.arguments) {
-	    		console.log("[DEBUG]: Arguments: ", input.arguments);
-	    	}
-    	}
+		if (debug) {
+			console.log("[DEBUG]: Running cli tool: ", input.command);
+			if (input.arguments) {
+				console.log("[DEBUG]: Arguments: ", input.arguments);
+			}
+		}
 
 		return new Promise((resolve) => {
 			let command = input.command;
@@ -52,20 +37,23 @@ export const tools: ToolDefinition[] = [
 			}
 
 			exec(command, (error: Error | null, stdout: string, stderr: string) => {
-			  if (error) {
-			  	console.log("ERROR: ", error.message)
-			    console.log("Returning: " + `Unexpected error running command!`);
-			    return resolve(`Unexpected error running command!`);
-			  }
-			  if (stderr) {
-			    // console.log("Returning: " + `Stderr: ${stderr}`);
-			    return resolve(`Stderr: ${stderr}`);
-			  }
-			  // console.log("Returning: " + stdout);
-			  return resolve(stdout);
+				if (error) {
+				console.log("ERROR: ", error.message)
+				console.log("Returning: " + `Unexpected error running command!`);
+				return resolve(`Unexpected error running command!`);
+
+				} else if (stderr) {
+				let errMessage = "Error running command \"" + command + "\" - consider running `tldr {command}` for usage examples."
+				errMessage += "STDERR: " + stderr;
+
+				return resolve(errMessage);
+
+				} else {
+				return resolve(stdout);
+				}
 			});
 		});
-    },
-  },
+	},
+	},
 
 ];
